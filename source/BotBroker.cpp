518953f228860a19a41581c08a858b564671175a
tomasmark79 <tomas@digitalspace.name>
2024-12-04 09:50:12 +0100

    crosscompile preparation

--- source/BotBroker.cpp
+++ source/BotBroker.cpp
@@ -1,17 +1,25 @@
 #include "MyDiscordBot/BotBroker.hpp"
 
-#include <dpp/dpp.h>
 #include "emoji/EmojiWrapper.hpp"
 
 #include <atomic>
 #include <chrono>
+#include <curl/curl.h>
+#include <dpp/dpp.h>
+#include <fstream>
 #include <iostream>
 #include <thread>
 #include <unordered_map>
-#include <fstream>
 
 std::atomic<bool> stopTimerThread(false);
 Emoji /*💋*/ emojiWrapper;
+
+// Funkce pro zápis dat z libcurl do std::string
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string *)userp)->append((char *)contents, size * nmemb);
+    return size * nmemb;
+}
 
 BotBroker::BotBroker()
 {
@@ -21,7 +29,6 @@ BotBroker::BotBroker()
     {
         std::getline(tokenFile, BOT_TOKEN);
         tokenFile.close();
-
     }
     else
     {
@@ -57,6 +64,38 @@ BotBroker::BotBroker()
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             }
 
+            if (event.command.get_command_name() == "devizovekurzy")
+            {
+
+                CURL *curl;
+                CURLcode res;
+                std::string readBuffer;
+                // sudo apt install libcurl4
+                curl = curl_easy_init();
+                if (curl)
+                {
+                    curl_easy_setopt(curl, CURLOPT_URL,
+                                     "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/"
+                                     "kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt");
+                    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+                    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+                    res = curl_easy_perform(curl);
+                    if (res != CURLE_OK)
+                    {
+                        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res)
+                                  << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "Downloaded content:\n" << readBuffer << std::endl;
+                    }
+                    curl_easy_cleanup(curl);
+                }
+
+                event.reply("Ziskat devizove kurzy!");
+            }
+
             if (event.command.get_command_name() == "stopemojitimer")
             {
                 stopTimerThread.store(true);
@@ -68,12 +107,11 @@ BotBroker::BotBroker()
                 std::cout << "startemojitimer" << std::endl;
                 std::string randomEmoji;
                 emojiWrapper.getRandomEmoji(randomEmoji);
-              
-                
+
                 // reply required
                 dpp::message msg(event.command.channel_id, "OK");
                 event.reply(msg);
-                
+
                 std::this_thread::sleep_for(std::chrono::seconds(1));
 
                 stopTimerThread.store(false);
@@ -83,7 +121,7 @@ BotBroker::BotBroker()
                         while (!stopTimerThread.load())
                         {
                             std::cout << ". Tick ." << std::endl;
-                            
+
                             std::string randomEmoji;
                             emojiWrapper.getRandomEmoji(randomEmoji);
 
@@ -119,7 +157,6 @@ BotBroker::BotBroker()
 
                 bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
 
-
                 bot.global_command_create(
                     dpp::slashcommand("emoji", "Show Random Emoji!", bot.me.id));
 
@@ -128,14 +165,13 @@ BotBroker::BotBroker()
 
                 bot.global_command_create(
                     dpp::slashcommand("stopemojitimer", "Stop Random Emoji Timer!", bot.me.id));
+
+                bot.global_command_create(
+                    dpp::slashcommand("devizovekurzy", "Ziskat devizove kurzy!", bot.me.id));
             }
         });
 
     bot.start(dpp::st_wait);
-
 }
 
-BotBroker::~BotBroker()
-{
-
-}
+BotBroker::~BotBroker() {}
