c13a02096973ff9de1824329c7db71fa78bcab8a
tomasmark79 <tomas@digitalspace.name>
2024-12-04 11:08:42 +0100

    local openssl, opus library compilation for future crosscompilations

--- rpi4-toolchain.cmake
+++ rpi4-toolchain.cmake
@@ -1,18 +1,17 @@
-# todo
-
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# C and C++ compilers for the target
+# Cesty ke crosskompilátoru
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
+set(CMAKE_LINKER aarch64-linux-gnu-ld)
 
-# Path to the Raspberry Pi sysroot
-set(CMAKE_SYSROOT /home/tomas/dev/rpi/)
-
-# Linker and include paths
-set(CMAKE_FIND_ROOT_PATH /home/tomas/dev/rpi/)
+# Rootfs pro knihovny a hlavičky
+set(CMAKE_SYSROOT /home/tomas/rpi-rootfs)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Architektura
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a --sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a --sysroot=${CMAKE_SYSROOT}")
