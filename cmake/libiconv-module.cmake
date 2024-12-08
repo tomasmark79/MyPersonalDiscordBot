e65e5e89a196a00e0fb7c6a6bf5579db72ae9d29
tomasmark79 <tomas@digitalspace.name>
2024-12-08 22:57:40 +0100

    refactored template library + module template

--- cmake/libiconv-module.cmake
+++ cmake/libiconv-module.cmake
@@ -1,3 +1,5 @@
+# important - Ninja does not work with this code project
+
 function(add_libiconv)
     CPMAddPackage(
         NAME libiconv
@@ -6,15 +8,16 @@ function(add_libiconv)
     )
 
     if(libiconv_ADDED)
+        set(LIBICONV_BUILD_DIR ${libiconv_BINARY_DIR})
+        file(MAKE_DIRECTORY ${LIBICONV_BUILD_DIR})
         set(LIBICONV_SOURCE_DIR ${libiconv_SOURCE_DIR})
         set(LIBICONV_INSTALL_DIR ${CMAKE_BINARY_DIR}/_deps/libiconv-install)
-        set(LIBICONV_BUILD_DIR ${libiconv_BINARY_DIR})
-
-        file(MAKE_DIRECTORY ${LIBICONV_BUILD_DIR})
+        set(LIBICONV_LIBRARY ${LIBICONV_INSTALL_DIR}/lib/libiconv.a)
 
         if(CMAKE_CROSSCOMPILING)
             add_custom_command(
-                OUTPUT ${LIBICONV_INSTALL_DIR}/lib/libiconv.a
+                WORKING_DIRECTORY ${LIBICONV_SOURCE_DIR}
+                OUTPUT ${LIBICONV_LIBRARY}
                 COMMAND ./configure
                 --host=${CROSS_HOST}
                 --prefix=${LIBICONV_INSTALL_DIR}
@@ -26,29 +29,24 @@ function(add_libiconv)
                 CXXCPP=${CMAKE_CXX_COMPILER}\ -E
                 COMMAND ${CMAKE_MAKE_PROGRAM} -j$(nproc)
                 COMMAND ${CMAKE_MAKE_PROGRAM} install
-                WORKING_DIRECTORY ${LIBICONV_SOURCE_DIR}
-                COMMENT "CrossBuild libiconv downloaded from the internet"
-
+                COMMENT "CrossBuilding libiconv"
             )
         else() # Native build
             add_custom_command(
-                OUTPUT ${LIBICONV_INSTALL_DIR}/lib/libiconv.a
-                COMMAND ./configure
-                --prefix=${LIBICONV_INSTALL_DIR}
-                --enable-shared
-                --enable-static
-                COMMAND ${CMAKE_MAKE_PROGRAM} -j$$(nproc)
-                COMMAND ${CMAKE_MAKE_PROGRAM} install
                 WORKING_DIRECTORY ${LIBICONV_SOURCE_DIR}
-                COMMENT "Building libiconv downloaded from the internet"
+                OUTPUT ${LIBICONV_LIBRARY}
+                COMMAND ./configure --prefix=${LIBICONV_INSTALL_DIR} --enable-shared --enable-static
+                COMMAND ./config.status
+                COMMAND make -j$$(nproc)
+                COMMAND make -j$$(nproc) install
+                COMMAND libtool --finish ${LIBICONV_INSTALL_DIR}
+                COMMENT "Building libiconv"
             )
         endif()
 
-        add_custom_target(build_libiconv DEPENDS ${LIBICONV_INSTALL_DIR}/lib/libiconv.a)
+        add_custom_target(build_libiconv DEPENDS ${LIBICONV_LIBRARY})
 
         set(LIBICONV_INCLUDE_DIR ${LIBICONV_INSTALL_DIR}/include)
-        set(LIBICONV_LIBRARY ${LIBICONV_INSTALL_DIR}/lib/libiconv.a)
-        set(LIBICONV_SHARED_LIBRARY ${LIBICONV_INSTALL_DIR}/lib/libiconv.so)
 
         include_directories(${LIBICONV_INCLUDE_DIR})
         link_directories(${LIBICONV_INSTALL_DIR}/lib)
