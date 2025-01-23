633d7a90eb1f371cb1f5d8d8e55d84dcd66722a6
tomasmark79 <tomas@digitalspace.name>
2025-01-23 16:58:57 +0100

    vcmake upgraded

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,34 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-class CompressorRecipe(ConanFile):
+class MarkWareVCMake(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
-
     def requirements(self):
-        self.requires("zlib/1.2.11")
-        self.requires("fmt/11.0.2")
-        self.requires("nlohmann_json/3.11.2")
-        self.requires("libcurl/8.10.1")
+        self.requires("zlib/1.2.11") # used to compress and decompress data
+        self.requires("fmt/11.1.1") # required by cpm package
+        self.requires("nlohmann_json/3.11.2") # is modern to have json support
+    # it is on you to define rest of the dependencies
         self.requires("dpp/10.0.35")
+
+    # -------------------------------------- --
+    # MarkWareVCMake is using self mechanism --
+    # to define build ouput layout!          --
+    # ├── Build
+    #     ├── Artefacts - tarballs
+    #     ├── Install - final installation
+    #     ├── Library - library build
+    #     └── Standalone - standalone build
+    # ├── cmake
+    # ├── include
+    # ├── Source
+    # └── Standalone
+    # ------------------------keep-commented --
+    # def layout(self):                      --
+        # cmake_layout(self)                 --
+    # -------------------------------------- --
+
+       
+
+        
